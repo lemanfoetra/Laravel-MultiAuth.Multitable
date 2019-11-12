535aa9adf9e00a70cb0d497e3b1b4ead13f9c69c
Ardyn Sulaeman <lemanfoetra@gmail.com>
2019-11-12 09:00:20 +0700

    first commit

---
+++ app/Http/Controllers/AuthAdmin/LoginController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\AuthAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+
+    public function showLoginForm()
+    {
+        return view('authAdmin.login');
+    }
+
+
+    public function login(Request $request)
+    {   
+
+        $request->validate([
+            'email'  => 'required|string',
+            'password'  => 'required|min:6',
+        ]); 
+
+
+        $credential = [
+            'email'     => $request->email,
+            'password'  => $request->password,
+        ];
+
+
+        if(Auth::guard('admin')->attempt($credential, $request->member)){
+
+            return redirect()->intended(route('admin.home'));
+        }else{
+            echo "false";
+        }
+
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect(route('admin.home'));
+    }
+
+
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+
+}
